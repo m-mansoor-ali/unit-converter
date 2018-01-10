d2bf950be4fd667f58bb9f6e78d347bf52c29db5
M Mansoor Ali <m.mansoor.ali@outlook.com>
2018-01-10 19:28:30 +0500

    units data seperated

---
+++ src/unit-converter-app/units.js
@@ -0,0 +1,38 @@
+var units = [
+    {
+        "name": "Time",
+        "icon": "custom:clock"
+    },
+    {
+        "name": "Length",
+        "icon": "custom:ruler"
+    },
+    {
+        "name": "Temperature",
+        "icon": "custom:thermometer-lines"
+    },
+    {
+        "name": "Speed",
+        "icon": "custom:speedometer"
+    },
+    {
+        "name": "Mass",
+        "icon": "custom:scale-balance"
+    },
+    {
+        "name": "Area",
+        "icon": "custom:cube-outline"
+    },
+    {
+        "name": "Volume",
+        "icon": "custom:flask-outline"
+    },
+    {
+        "name": "Digital Storage",
+        "icon": "custom:floppy"
+    },
+    {
+        "name": "Fuel",
+        "icon": "custom:fuel"
+    }
+];
